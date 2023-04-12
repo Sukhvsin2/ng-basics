03d867879d214e141f340acfab626eaadb783859
Sukhvinder Singh <sukhvindersingh@Sukhvinders-MacBook-Air.local>
2023-04-12 14:08:25 -0400

    fake backend set

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,16 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { Task } from '../../Task';
-import { TASKS } from '../..//mock-task';
+import {TaskService} from "../../services/task.service"
+
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
   styleUrls: ['./tasks.component.scss']
 })
 export class TasksComponent implements OnInit {
-  tasks: Task[] = TASKS;
-  constructor() { }
+  tasks: Task[] = [];
+  constructor(private taskService: TaskService) { }
 
   ngOnInit(): void {
+    this.taskService.getTasks().subscribe(tasks => this.tasks = tasks);
   }
 
 }
